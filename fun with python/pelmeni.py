c61f82a46f6d385b13c4227695d7bc6ac84ba404
T1moHTML <timon.black.gamig@gmail.com>
2025-05-02 18:24:50 +0200

    Changed pelmeni.py

--- fun with python/pelmeni.py
+++ fun with python/pelmeni.py
@@ -1,10 +1,21 @@
-pelmeni = input("Did you cook the pelmeni? ").strip().lower()  # Приведення до нижнього регістра
+A = "OK"
+B = "Well, do you have anything else?:  "
+C = "Well, then I guess you need to buy kebab for dinner."
+D = "Well then cook it you lazy a**".upper()
+# Here we have all my variables
+
+pelmeni = input("Did you cook the pelmeni? ").lower()
+# This is an input field
+
 if pelmeni == "yes":
-    print("OK")
-    eat = input("Did you eat all of the pelmeni? ").strip().lower()
-    if eat == "yes":
-        print("Well, OK, then buy kebab for dinner")
+    print(A)
+elif pelmeni == "no":
+    second_food = input(B).lower()
+    if second_food == "no":
+        print(C)
+    elif second_food == "yes":
+        print(D)
     else:
-        print("ok then fry em' for dinner")
+        print("Please answer 'yes' or 'no'.")
 else:
-    print("Well, you should have cooked the pelmeni")
+    print("Please answer 'yes' or 'no'.")
