51d52a4d3ed5b24327316e76efe29ac349aa050b
T1mohtml <timon.black.gamig@gmail.com>
2025-05-10 08:53:56 +0200

    Merge remote and local changes

--- Hello_World/obj/Debug/net9.0/Hello_World.AssemblyInfo.cs
+++ Hello_World/obj/Debug/net9.0/Hello_World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991e7358f86ef247eee3c3a548e6354b423ca432")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
